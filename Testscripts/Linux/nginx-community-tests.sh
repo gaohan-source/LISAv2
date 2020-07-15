4a8ea899cacd63362864547fe459552be9373d55
gaohan-source <67756251+gaohan-source@users.noreply.github.com>
2020-07-14 22:05:11 -0700

    code revise

--- Testscripts/Linux/nginx-community-tests.sh
+++ Testscripts/Linux/nginx-community-tests.sh
@@ -14,9 +14,6 @@
 #   Test::Nginx Protocol::WebSocket
 #   IO::Socket::SSL Cache::Memcached
 #   Cache::Memcached::Fast
-
-# Function Timeout().
-# Avoiding the nginx tests timeout and stuck.
 #
 ###############################################################################
 
@@ -40,29 +37,12 @@ common_packages=(m4 bison flex make gcc psmisc autoconf automake)
 update_repos
 install_package "${common_packages[@]}"
 
-drive_name=$(bash get_data_disk_dev_name.sh)
-LogMsg "Disk used: $drive_name"
-
 case $DISTRO in
-    "suse"*)
-        suse_packages=(git-core db48-utils libaio-devel libattr1 \
-            libcap-progs libdb-4_8 perl-BerkeleyDB)
-        install_package "${suse_packages[@]}"
-        ;;
-    "ubuntu"* | "debian"*)
+    "ubuntu"*)
         deb_packages=(git libaio-dev libattr1 libcap-dev keyutils \
             libdb4.8 libberkeleydb-perl expect dh-autoreconf gdb \
             libnuma-dev quota genisoimage db-util unzip exfat-utils)
         install_package "${deb_packages[@]}"
-        ;;
-    "redhat"* | "centos"* | "fedora"*)
-		rpm_packages=(git libaio-devel libattr libcap-devel libdb)
-		# this must be revised later once epel_8 is available
-		if [[ $DISTRO != redhat_8 ]]; then
-			rpm_packages+=(db4-utils)
-		fi
-		install_epel
-        install_package "${rpm_packages[@]}"
         ;;
     *)
         LogMsg "Unknown distro $DISTRO, continuing to try for RPM installation"
@@ -86,32 +66,43 @@ Timeout()
 #pre-set the env
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
+nginx_git_url="https://github.com/nginx/nginx.git"
+nginxtest_git_url="https://github.com/nginx/nginx-tests.git"
 
 cd /home/lisa/;
 rm -Rf nginx
 rm -Rf nginx-tests
 
-git clone --depth 1 https://github.com/nginx/nginx.git
-git clone --depth 1 https://github.com/nginx/nginx-tests.git
+git clone --depth 1 nginx_git_url
+git clone --depth 1 nginxtest_git_url
 
 # compile and install the nginx via the config.test
 cd $SCRIPTPATH;
 
-cp nginx-config.sh /home/lisa/;
-cd /home/lisa/;
-chmod +x nginx-config.sh ;
+./auto/configure \
+--with-http_ssl_module \
+--with-http_slice_module \
+--with-pcre-jit \
+--with-threads \
+--with-http_v2_module \
+--without-http_fastcgi_module \
+--without-http_grpc_module \
+--without-http_uwsgi_module \
+--without-http_scgi_module \
+--without-http_memcached_module
 
 #run the test
 Timeout ;
 cd $SCRIPTPATH;
-RESULT1=$(tail -1  $SCRIPTPATH/nginx-test.log | grep "FAIL" | wc -l)
-if (( $RESULT1 ))
+Result1=$(tail -1  $SCRIPTPATH/nginx-test.log | grep "FAIL" | wc -l)
+if (( $Result1 ))
 then
         echo -e "Nginx Official Test RESULT:FAIL"
+        SetTestStateFailed() 
 else
         echo -e "Nginx Official Test RESULT:PASS"
+        SetTestStatePass() 
 fi
 
-collect_VM_properties
 SetTestStateCompleted
 exit 0;
